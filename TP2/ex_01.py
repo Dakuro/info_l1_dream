15b0383537f07fe6e583e6537d4a42bf84633812
Dakuro <51865183+Dakuro@users.noreply.github.com>
2021-11-21 15:35:10 +0100

    Update ex_01.py

--- TP2/ex_01.py
+++ TP2/ex_01.py
@@ -1,5 +1,19 @@
 import numpy as np
 
-nb_ints = int(input("Entrer un nombre d'entiers à saisir :\n"))
-array_nb = np.zeros(nb_ints, type=int)
-print(array_nb)
+nb_int = int(input("Entrer un nombre d'entiers à saisir :\n"))
+
+while nb_int < 1:
+    print("Valeur saisie incorrecte.")
+    nb_int = int(input("Entrer un nombre d'entiers à saisir :\n"))
+
+array_int = np.empty(nb_int, dtype=int)
+array_int[0] = int(input("Entrer l'entier n°{num} :\n".format(num=1)))
+smallest = array_int[0]
+
+for i in range(nb_int-1):
+    array_int[i+1] = int(input("Entrer l'entier n°{num} :\n".format(num=i+2)))
+    if array_int[i+1] < smallest:
+        smallest = array_int[i+1]
+
+print(array_int)
+print("L'entier le plus petit est", smallest)
