246cd00422ab69759b0ac2a7feedb73f7cd66cfb
Dakuro <51865183+Dakuro@users.noreply.github.com>
2021-11-24 11:28:22 +0100

    Add L1_TP3.pdf

--- TP2/dumb_version/ex_03.py
+++ TP2/dumb_version/ex_03.py
@@ -20,3 +20,14 @@ while not input_end:
         index += 1
 
 print(array_char)
+
+array_voy: TAB = zeros(6, str)
+array_voy[0], array_voy[1], array_voy[2], array_voy[3], array_voy[4], array_voy[5]\
+    = 'a', 'e', 'i', 'o', 'u', 'y'
+count_voy = int(0)
+count_con = int(0)
+
+for i in array_char:
+    for j in array_voy:
+        if array_char[i] == array_voy[j]:
+
